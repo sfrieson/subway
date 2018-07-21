4ea87f2af3aff6f93a6cfb5c8638b1d4d8250e74
Steven Frieson <sfrieson@gmail.com>
2018-07-21 13:34:32 -0400

    Add db field constraints

--- tasks/db-tables-create.sql
+++ tasks/db-tables-create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE agency (
   -- ALL LENGTHS ARE EXACT
-  agency_id CHAR(8),
+  agency_id CHAR(8) PRIMARY KEY,
   agency_name CHAR(25),
   agency_url CHAR(19),
   agency_timezone CHAR(16),
@@ -8,14 +8,8 @@ CREATE TABLE agency (
   agency_phone CHAR(12)
 );
 
-CREATE TABLE calendar_dates (
-  service_id VARCHAR(32), -- calendar.service_id
-  date DATE,
-  exception_type SMALLINT
-);
-
 CREATE TABLE calendar (
-  service_id VARCHAR(32), -- ESTIMATE
+  service_id VARCHAR(32) PRIMARY KEY, -- ESTIMATE
   monday BOOLEAN,
   tuesday BOOLEAN,
   wednesday BOOLEAN,
@@ -27,8 +21,14 @@ CREATE TABLE calendar (
   end_date DATE
 );
 
+CREATE TABLE calendar_dates (
+  service_id VARCHAR(32) REFERENCES calendar (service_id),
+  date DATE,
+  exception_type SMALLINT
+);
+
 CREATE TABLE routes (
-  route_id VARCHAR(5), -- ESTIMATE
+  route_id VARCHAR(5)  PRIMARY KEY, -- ESTIMATE
   agency_id VARCHAR(12), -- EXACT
   route_short_name VARCHAR(5), -- ESTIMATE
   route_long_name VARCHAR(32), -- ESTIMATE
@@ -40,27 +40,15 @@ CREATE TABLE routes (
 );
 
 CREATE TABLE shapes (
-  shape_id CHAR(8),
+  shape_id CHAR(8) PRIMARY KEY,
   shape_pt_lat FLOAT,
   shape_pt_lon FLOAT,
   shape_pt_sequence SMALLINT,
   shape_dist_traveled FLOAT -- UNSURE
 );
 
-CREATE TABLE stop_times (
-  trip_id VARCHAR(64), -- trips.trip_id
-  arrival_time TIME,
-  departure_time TIME,
-  stop_id VARCHAR(8), -- ESTIMATE
-  stop_sequence SMALLINT,
-  stop_headsign VARCHAR(32), -- UNSURE
-  pickup_type SMALLINT,
-  drop_off_type SMALLINT,
-  shape_dist_traveled FLOAT -- UNSURE
-);
-
 CREATE TABLE stops (
-  stop_id VARCHAR(8), -- ESTIMATE
+  stop_id VARCHAR(8) PRIMARY KEY, -- ESTIMATE
   stop_code VARCHAR(16), -- UNSURE
   stop_name VARCHAR(64), -- ESTIMATE
   stop_desc TEXT, -- UNSURE
@@ -69,22 +57,34 @@ CREATE TABLE stops (
   zone_id VARCHAR(8), -- UNSURE
   stop_url VARCHAR(64), -- UNSURE
   location_type SMALLINT,
-  parent_station VARCHAR(8) -- stop_id?
+  parent_station VARCHAR(8) REFERENCES stops (stop_id)
 );
 
 CREATE TABLE transfers (
-  from_stop_id VARCHAR(8), -- stops.stop_id
-  to_stop_id VARCHAR(8), -- stops.stop_id
+  from_stop_id VARCHAR(8) REFERENCES stops (stop_id),
+  to_stop_id VARCHAR(8) REFERENCES stops (stop_id),
   transfer_type SMALLINT,
   min_transfer_time SMALLINT -- seconds?
 );
 
 CREATE TABLE trips (
-  route_id VARCHAR(5), -- routes.route_id
-  service_id VARCHAR(32), -- calendar.service_id
-  trip_id VARCHAR(64),
+  route_id VARCHAR(5) REFERENCES routes (route_id),
+  service_id VARCHAR(32) REFERENCES calendar (service_id),
+  trip_id VARCHAR(64) PRIMARY KEY,
   trip_headsign VARCHAR(64), -- ESTIMATE
   direction_id SMALLINT,
   block_id VARCHAR(8), -- UNSURE
-  shape_id CHAR(8) -- shapes.shape_id
+  shape_id CHAR(8) REFERENCES shapes (shape_id)
+);
+
+CREATE TABLE stop_times (
+  trip_id VARCHAR(64) REFERENCES trips (trip_id),
+  arrival_time TIME,
+  departure_time TIME,
+  stop_id VARCHAR(8) REFERENCES stops (stop_id), -- ESTIMATE
+  stop_sequence SMALLINT,
+  stop_headsign VARCHAR(32), -- UNSURE
+  pickup_type SMALLINT,
+  drop_off_type SMALLINT,
+  shape_dist_traveled FLOAT -- UNSURE
 );
