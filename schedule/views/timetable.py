99871e170018b9bbbef56b28109d64fd8fb771d0
Steven Frieson <sfrieson@gmail.com>
2018-08-05 07:30:51 -0400

    Refactor SVG class

--- schedule/views/timetable.py
+++ schedule/views/timetable.py
@@ -3,7 +3,17 @@ class SVG:
         self.width, self.height = artboard_size
         self.data_width, self.data_height = data_size
 
+        self.grid_color = '#999999'
+        self.styles = [('st0', 'fill: none; stroke: %s;' % self.grid_color)]
+        self.contents = []
 
+
+    def add(self, contents):
+        self.contents.append(contents)
+
+    def add_style(self, name, defs):
+        self.styles.append((name, defs))
+    
     def body(self, contents):
         return """
         <svg version="1.1" id="Layer_1"
@@ -22,7 +32,25 @@ class SVG:
 
         lines = [((v, 0), (v, self.height)) for v in verticals] 
         lines = lines + [((0, h), (self.width, h)) for h in horizontals]
-        return '<g id="grid-marks">%s</g>' % ' '.join([self.line(line, 'st0') for line in lines])
+        return self.group((
+            'grid-marks',
+            ''.join([self.line(line, 'st0') for line in lines])
+        ))
+
+    def group(self, data):
+        return '<g id="%s">%s</g>' % data
+
+    def line(self, line, classname=None):
+        p1, p2 = line
+        x1, y1 = p1
+        x2, y2 = p2
+        return '<line {classname} x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" />'.format(
+            x1 = x1,
+            x2 = x2,
+            y1 = y1,
+            y2 = y2,
+            classname = 'class="%s"' % classname if classname else ''
+        )
 
     def normalize_x(self, x):
         return x / self.data_width * self.width
@@ -38,43 +66,29 @@ class SVG:
     <!-- Generator: Adobe Illustrator 22.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
     """
 
-    style = """<style type="text/css">
-            .st0 {{
-                fill: none;
-                stroke: {grid_color};
-            }}
-            .st1 {{
-                fill: none;
-                stroke: {path_color};
-                stroke-width: 2;
-            }}
-        </style>""".format(
-            grid_color = "#999999",
-            path_color = "#000000"
-        )
-
-    def line(self, line, classname):
-        p1, p2 = line
-        x1, y1 = p1
-        x2, y2 = p2
-        return '<line class="{classname}" x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" />'.format(
-            x1 = x1,
-            x2 = x2,
-            y1 = y1,
-            y2 = y2,
-            classname = classname
-        )
-
     def normalized_line(self, line, classname):
-        
         p1, p2 = line
         return self.line(
             (self.normalize_point(p1), self.normalize_point(p2)),
             classname
         )
 
+    def print(self):
+        return """{header}
+        {body}""".format(
+            header = self.header,
+            body = self.body([
+                    '<style type="text/css">',
+                    ''.join(['.%s { %s }' % style for style in self.styles]),
+                    '</style>',
+                    self.grid(30 * 60, 1)
+                ] + self.contents
+            )
+        )
+
+
 def draw(route):
-    pen = SVG((1720, 1080), (24 * 60 * 60, len(route.get_stations())))
+    drawing = SVG((1720, 1080), (24 * 60 * 60, len(route.get_stations())))
 
     trip_lines = []
     for trip in route.trips:
@@ -84,13 +98,15 @@ def draw(route):
                 (segment.end.arrival_time.value, segment.end.station.line_order))
             )
 
-    return """{header}
-    {body}""".format(
-        header = pen.header,
-        body = pen.body([
-            pen.style,
-            pen.grid(30 * 60, 1),
-            '<g id="routes">%s</g>' % \
-                ''.join([pen.normalized_line(line, 'st1') for line in trip_lines])
-        ])
-    )
+    drawing.add_style('st1', """
+        fill: none;
+        stroke: %s;
+        stroke-width: 2;
+    """ % '#000000')
+
+    drawing.add(drawing.group((
+        'routes',
+        ''.join([drawing.normalized_line(line, 'st1') for line in trip_lines])
+    )))
+    return drawing.print()
+
